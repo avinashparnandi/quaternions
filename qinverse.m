e9d80ae2ad4a904af103052e81c1f74420f361d8
Avinash <47569326+avinashparnandi@users.noreply.github.com>
2019-04-17 09:07:19 -0400

    Add files via upload

---
+++ qinverse.m
@@ -0,0 +1,12 @@
+function ret = qinverse( q )
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+% This simply 
+
+ret(1) = q(1);
+ret(2) = -1*q(2);
+ret(3) = -1*q(3);
+ret(4) = -1*q(4);
+
+end
+
